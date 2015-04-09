e99e3534d2fb4bac2484f53e3f4cc35413365850
Rolf Erik Lekang <me@rolflekang.com>
2015-04-09 19:15:52 +0200

    Add check of dispatcher_url to fetcher while condition

--- frigg_worker/fetcher.py
+++ frigg_worker/fetcher.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 def fetcher(**options):
     options = evaluate_options(options)
-    while True:
+    while options['dispatcher_url']:
         task = fetch_task(options['dispatcher_url'], options['dispatcher_token'])
         if task:
             start_build(task, options)
